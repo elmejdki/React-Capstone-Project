648a44d886d5e8e092e0ddb3b333109dddf86997
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-01-08 01:28:55 +0100

    Add Filters Reducer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,6 +4,7 @@ import {
 import thunk from 'redux-thunk';
 import categoriesReducer from '../reducers/categories';
 import mealsReducer from '../reducers/meals';
+import filtersReducer from '../reducers/filters';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -12,6 +13,7 @@ export default () => {
     combineReducers({
       meals: mealsReducer,
       categories: categoriesReducer,
+      filters: filtersReducer,
     }),
     composeEnhancers(applyMiddleware(thunk)),
   );
