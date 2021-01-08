e89d44bb9cf774e367d89caa71ec59c674b714c6
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-01-08 01:15:03 +0100

    Add categories reducer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,6 +2,7 @@ import {
   createStore, combineReducers, applyMiddleware, compose,
 } from 'redux';
 import thunk from 'redux-thunk';
+import categoriesReducer from '../reducers/categories';
 import mealsReducer from '../reducers/meals';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -10,6 +11,7 @@ export default () => {
   const store = createStore(
     combineReducers({
       meals: mealsReducer,
+      categories: categoriesReducer,
     }),
     composeEnhancers(applyMiddleware(thunk)),
   );
