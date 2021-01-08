027df1ddc565ebb69a689f682860205334458ed2
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-01-09 00:01:01 +0100

    Style the not found page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './NotFoundPage.css';
 
 const NotFoundPage = () => (
   <div className="page-container">
-    <h1>404 Page</h1>
+    <div
+      className="not-found-page"
+    >
+      <h1>404</h1>
+      <p>Not Found!</p>
+    </div>
   </div>
 );
 
